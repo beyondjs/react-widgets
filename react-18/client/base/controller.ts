b1bafa79403e9341d83ef02a63a52b05c7687c00
henrybox <ebox@socites.com>
2022-12-31 12:38:56 -0300

    New package/project dependencies method configuration updated

--- react-18/client/base/controller.ts
+++ react-18/client/base/controller.ts
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from 'react';
 import {createRoot, Root, hydrateRoot} from 'react-dom/client';
 import {WidgetClientController} from '@beyond-js/widgets/controller';
 import Widget from './widget';
