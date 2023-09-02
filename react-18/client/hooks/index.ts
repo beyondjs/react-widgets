0295a09a22e76af024f1d07392d76438ef98ef00
Bryant <90587911+veD-tnayrB@users.noreply.github.com>
2023-09-01 23:59:28 -0400

    Now the useBinder hook can accept multiples events or just one 🔥

--- react-18/client/hooks/index.ts
+++ react-18/client/hooks/index.ts
@@ -1,5 +1,4 @@
-import * as React from 'react';
-
+import * as React from "react";
 /***
  * Executes a useEffect hook binging the event defined in all
  * objects passed
@@ -8,13 +7,18 @@ import * as React from 'react';
  * @param {function} onBinder function to be executed when the event is fired
  * @param {string} event the event to be listened, by default is event change
  */
-export /*bundle*/
-function useBinder(objects: any[], onBinder: () => void, event: string = 'change'): void {
-    React.useEffect((): (() => void) => {
-        objects.forEach((object: any): void => {
-            if (!object) throw new Error(`object is not valid in useBinder ${object}`);
-            object.on(event, onBinder);
+ export /*bundle*/
+ function useBinder(objects: any[], onBinder: () => void, events: string | string[] = 'change'): void {
+   const bindEvents : string[] = typeof events === 'string' ? [events] : events;
+    React.useEffect(() => {
+        objects.forEach(object => {
+            if(!object?.bind) return
+            bindEvents.forEach(event => object.bind(event, onBinder));
         });
-        return (): void => objects.forEach((object: any) => object.off(event, onBinder));
+       
+        return () => objects.forEach(object => {
+            if(!object?.unbind) return
+            bindEvents.forEach(event => object.unbind(event, onBinder));
+        });
     }, []);
 }
