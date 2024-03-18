8a7305f42ec0639b7c664aa3caf50243fac9b290
ftovar8 <felix.godo@gmail.com>
2024-03-18 14:05:11 -0300

    feat: use texts updated

--- react-18/ssr/page/page.ts
+++ react-18/ssr/page/page.ts
@@ -1,5 +1,10 @@
-import {ReactWidgetController} from "@beyond-js/react-18-widgets/base";
+import { ReactWidgetController } from '@beyond-js/react-18-widgets/base';
+import { PageURI } from '@beyond-js/widgets/routing';
 
 export /*bundle*/
 abstract class PageReactWidgetController extends ReactWidgetController {
+	#uri: PageURI;
+	get uri() {
+		return this.#uri;
+	}
 }
