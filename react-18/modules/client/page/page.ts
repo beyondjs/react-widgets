6f2782011d9e5c394580415fce84822df0d9e549
Henry Box <14365005+henrybox@users.noreply.github.com>
2024-03-18 20:37:51 +0000

    feat: onQueryString method added to page controller

--- react-18/modules/client/page/page.ts
+++ react-18/modules/client/page/page.ts
@@ -1,9 +1,11 @@
+import type { URI } from '@beyond-js/kernel/routing';
 import type { PageURI } from '@beyond-js/widgets/routing';
+import type { IPageWidgetController } from '@beyond-js/widgets/controller';
 import { ReactWidgetController } from '@beyond-js/react-18-widgets/base';
 import { manager } from '@beyond-js/widgets/routing';
 
 export /*bundle*/
-abstract class PageReactWidgetController extends ReactWidgetController {
+abstract class PageReactWidgetController extends ReactWidgetController implements IPageWidgetController {
 	#uri: PageURI;
 	get uri() {
 		return this.#uri;
@@ -13,9 +15,14 @@ abstract class PageReactWidgetController extends ReactWidgetController {
 		return super.mount({ uri: this.#uri });
 	}
 
+	onQueryString(qs: URI['qs']) {
+		void qs;
+	}
+
 	async initialise() {
 		const { widget } = this;
 		const uri = manager.pages.obtain({ widget });
+		uri.on('change', this.onQueryString.bind(this));
 		this.#uri = uri;
 
 		await super.initialise();
