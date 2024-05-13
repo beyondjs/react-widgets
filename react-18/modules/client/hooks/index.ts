473134a304183b4be1b820020f18c8bd409128d1
Julio Rodriguez <jircdev@gmail.com>
2024-05-13 11:36:47 -0300

    fix: Add correct type to onBinder function letting it to receive params from the event

--- react-18/modules/client/hooks/index.ts
+++ react-18/modules/client/hooks/index.ts
@@ -1,4 +1,5 @@
-import React from 'react';
+import * as React from 'react';
+import { BinderEvents, CallbackFunction, ReactiveModel } from './types';
 /***
  * Executes a useEffect hook binging the event defined in all
  * objects passed
@@ -7,18 +8,15 @@ import React from 'react';
  * @param {string} events the event to be listened, by default is event change
  */
 export /*bundle*/
-function useBinder(objects: any[], onBinder: () => void, events: string | string[] = 'change'): void {
+function useBinder(objects: ReactiveModel[], onBinder: CallbackFunction, events: BinderEvents = 'change'): void {
 	const bindEvents: string[] = typeof events === 'string' ? [events] : events;
-	React.useEffect(() => {
-		objects.forEach(object => {
-			if (!object?.on) return;
-			bindEvents.forEach(event => object.on(event, onBinder));
-		});
 
-		return () =>
-			objects.forEach(object => {
-				if (!object?.off) return;
-				bindEvents.forEach(event => object.off(event, onBinder));
-			});
+	React.useEffect(() => {
+		const callback = (object: ReactiveModel, method: 'on' | 'off') => {
+			if (!object[method]) return;
+			bindEvents.forEach(event => object.on(event, onBinder));
+		};
+		objects.forEach(object => callback(object, 'on'));
+		return () => objects.forEach(object => callback(object, 'off'));
 	}, []);
 }
